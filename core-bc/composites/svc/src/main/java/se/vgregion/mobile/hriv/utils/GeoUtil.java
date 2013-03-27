b4d847c5f822ec00495a2bca4c0ef67bc670df0a
patrik.bergstrom <patrik.bergstrom@knowit.se>
2013-03-27 09:36:43 +0000

    More robust parsing of coordinates. Don't fail the whole request when one unit fails.

--- core-bc/composites/svc/src/main/java/se/vgregion/mobile/hriv/utils/GeoUtil.java
+++ core-bc/composites/svc/src/main/java/se/vgregion/mobile/hriv/utils/GeoUtil.java
@@ -1,6 +1,8 @@
 package se.vgregion.mobile.hriv.utils;
 
 
+import java.util.Scanner;
+
 /**
  * Utility-class for geographically related conversions etc.
  */
@@ -16,8 +18,9 @@ public abstract class GeoUtil {
         int[] result = null;
         if (!"".equals(rt90String)) {
             if (rt90String.indexOf("X:") >= 0 && rt90String.indexOf("Y:") >= 0) {
-                int rt90X = Integer.parseInt(rt90String.substring(3, 10));
-                int rt90Y = Integer.parseInt(rt90String.substring(15));
+                Scanner scanner = new Scanner(rt90String);
+                int rt90X = Integer.parseInt(scanner.findInLine("\\d+"));
+                int rt90Y = Integer.parseInt(scanner.findInLine("\\d+"));
                 result = new int[]{rt90X, rt90Y};
             }
         }
